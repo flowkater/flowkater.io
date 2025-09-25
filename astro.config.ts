b7b83b644465f41706736f7939ce7bb9c68e946b
flowkater <chjw102@gmail.com>
2025-09-25 19:43:14 +0900

    scrumble team interview release

--- astro.config.ts
+++ astro.config.ts
@@ -1,4 +1,5 @@
 import { defineConfig, envField } from "astro/config";
+import mdx from "@astrojs/mdx";
 import tailwindcss from "@tailwindcss/vite";
 import sitemap from "@astrojs/sitemap";
 import remarkToc from "remark-toc";
@@ -15,6 +16,7 @@ import { SITE } from "./src/config";
 export default defineConfig({
   site: SITE.website,
   integrations: [
+    mdx(),
     sitemap({
       filter: page => SITE.showArchives || !page.endsWith("/archives"),
     }),
