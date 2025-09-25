d82ef1010699bfc721f2e45fd5cca193af12c560
flowkater <chjw102@gmail.com>
2025-09-25 21:18:49 +0900

    og tag updated

--- scripts/new-post.mjs
+++ scripts/new-post.mjs
@@ -41,6 +41,7 @@ const fm = `---\n` +
   `canonicalURL: "${canonicalURL}"\n` +
   `tags:\n` +
   `  - "others"\n` +
+  `# ogImage: "/assets/og/sample.png" # (선택) 커스텀 OG 이미지 경로. 사용하지 않으면 이 줄을 삭제하세요.\n` +
   `featured: false\n` +
   `draft: true\n` +
   `slug: "${slug}"\n` +
